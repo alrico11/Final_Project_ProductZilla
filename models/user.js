490b518b890d161115f8a791cad86cd9aee85543
Alrico Rizki <alricosaiyan123@gmail.com>
2023-05-30 01:10:57 +0700

    Fix Users

--- models/user.js
+++ models/user.js
@@ -1,17 +1,16 @@
 const mongoose = require('mongoose');
+const uuid = require('uuid');
 
 const userSchema = new mongoose.Schema({
-    user_id: { type: String, required: true, unique: true },
+    user_id: { type: String, default: uuid.v4, unique: true },
     fullname: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     telephone: { type: String, required: true },
     birth_date: { type: Date, required: true },
     gender: { type: Boolean, default: null },
-    status_active: { type: Boolean, default: true },
-    created_at: { type: Date, default: Date.now },
-    modified_at: { type: Date, default: null }
-});
+    status_active: { type: Boolean, default: true }
+}, { timestamps: true });
 
 const User = mongoose.model('User', userSchema);
 module.exports = User;
